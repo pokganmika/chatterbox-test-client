e9a5853d80e3c9cadf531b4301ffb406c3671dfe
calmgust <calmgust@gmail.com>
2019-02-09 16:30:08 +0900

    add ui card

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <React.Fragment>
+        <>
           <Header/>
           {/* Route를 사용할 경우 props를 내리는 방법 but 문제가 생길 수도 있다!!*/}
           {/* <Route exact path='/' component={() => <Home username={this.state.username}/>}/> */}
@@ -27,7 +27,7 @@ class App extends Component {
           <Route path='/post' component={Chat} />
           <Route path='/users/login' component={Login}/>
           <Route path='/users/signup' component={Signup}/>
-        </React.Fragment>
+        </>
       </Router>
     );
   }
---
+++ src/components/Common/ChatCard.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+};
+
+function MediaCard(props) {
+  const { classes, name, text } = props;
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        {/* <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        /> */}
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {name}
+          </Typography>
+          <Typography component="p">
+            {text}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      {/* <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions> */}
+    </Card>
+  );
+}
+
+MediaCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(MediaCard);
